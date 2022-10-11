c18030614860c1b45e30c54f1e53641dfc19186a
KH Rasedul Islam <87899184+khrasedul-dev@users.noreply.github.com>
2022-10-11 22:15:12 +0600

    Update bot.js

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -11,12 +11,12 @@ bot.command('test',ctx=>{
     ctx.reply('This is test command')
 })
 
-exports.handler = async event =>{
+exports.handler = async event => {
     try {
-        await bot.handleUpdate(JSON.parse(event.body))
-        return {statusCode: 200, body: ""}
+      await bot.handleUpdate(JSON.parse(event.body))
+      return { statusCode: 200, body: "" }
     } catch (e) {
-        console.log(e)
-        return {statusCode: 404 , body: "Fail"}
+      console.error("error in handler:", e)
+      return { statusCode: 400, body: "This endpoint is meant for bot and telegram communication" }
     }
-}
+  }
